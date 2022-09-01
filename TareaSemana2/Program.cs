0ff06ce184e25c0e23ddb4404c8673f6ad447a67
Miguel Angel <miguel@gmail.com>
2022-08-31 22:01:42 -0500

    first commit

---
+++ TareaSemana2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaSemana2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Variables
+
+            int x1, y1;
+            int x2, y2;
+            int x3, y3;
+
+            //Ingresado por consola
+            Console.WriteLine("Ingrese la coordenada X del vertice A (?, y): ");
+            x1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la coordenada Y del vertice A (x, ?): ");
+            y1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la coordenada X del vertice B (?, y): ");
+            x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la coordenada Y del vertice B (x, ?): ");
+            y2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la coordenada X del vertice C (?, y): ");
+            x3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la coordenada Y del vertice C (x, ?): ");
+            y3 = int.Parse(Console.ReadLine());
+
+
+
+            // vertice A
+            Vertice v1 = new Vertice { x= x1, y = y1};
+            //Vertice B
+            Vertice v2 = new Vertice { x = x2, y = y2 };
+            //Vertice C
+            Vertice v3 = new Vertice { x = x3, y= y3};
+            //Vertice D
+            //Vertice v4 = new Vertice { x = 5, y=1};
+
+            Rectangulo rectangulo = new Rectangulo();
+
+            rectangulo.V1 = v1;
+            rectangulo.V2 = v2;
+            rectangulo.V3 = v3;
+            //rectangulo.v4 = v4;
+
+            Console.WriteLine("El área es: "+rectangulo.getArea());
+            Console.WriteLine("El perimetro es: "+rectangulo.getPerimetro());
+
+            Console.Read();
+        }
+    }
+}
---
+++ TareaSemana2/Rectangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaSemana2
+{
+    public class Rectangulo
+    {
+        public Vertice V1 { get; set; }
+        public Vertice V2 { get; set; }
+        public Vertice V3 { get; set; }
+        public Vertice V4 { get; set; }
+
+        public double getPerimetro()
+        {
+            double baseP = Math.Sqrt(Math.Pow(V2.x - V1.x, 2) + Math.Pow(V2.y - V1.y, 2));
+            double alturaP = Math.Sqrt(Math.Pow(V3.x - V2.x, 2) + Math.Pow(V3.y - V2.y, 2));
+            return 2*(baseP + alturaP);
+            
+        }
+
+        public int getArea()
+        {
+            //Para hallar una diagonal
+            //double base1 = Math.Sqrt(Math.Pow(v2.x - v1.x, 2) + Math.Pow(v2.y-v1.y, 2));
+            int baseR = V2.x- V1.x;
+            int alturaR = V2.y - V3.y;
+            int area = baseR * alturaR;
+            if (area > 0)
+            {
+                return area;
+            } else
+            {
+                return -1 * area;
+            }
+        }
+
+    }
+}
